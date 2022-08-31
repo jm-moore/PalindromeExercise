0d6c228954839be9ebe084073680d89f88844818
jm-moore <jmmoore9358@gmail.com>
2022-08-31 09:46:28 -0500

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string a)
+        {
+            return a.SequenceEqual(a.Reverse());
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,21 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("mom", true)]
+        [InlineData("dad", true)]
+        [InlineData("foot", false)]
+        [InlineData("tooth", false)]
+        [InlineData("mad", false)]
+        [InlineData("aaaaa", true)]
+        public void IsAPalindrome(string a, bool expected)
         {
+            var word = new WordSmith();
 
+            var actual = word.IsAPalindrome(a);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
